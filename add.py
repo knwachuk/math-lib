5555166878abed0b10e13fbf5cbe81e82ff43d1d
Alanna Combs <W211738705@student.hccs.edu>
2023-03-02 10:49:54 -0600

    Add addition module

---
+++ add.py
@@ -0,0 +1,13 @@
+"""Addition module."""
+
+def add(number_1: float, number_2: float) -> float: 
+    """Addition function
+
+    Args:
+        number_1 (float): addend.
+        number_2 (float): addend.
+
+    Returns:
+        float: sum of number_1 and number_2
+    """
+    return number_1 + number_2
