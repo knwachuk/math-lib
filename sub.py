5bda14c0baf780aa1ca1799fb0ac4d458aa0dad2
codenamejupiterx <behal1@morgan.edu>
2023-03-02 11:55:02 -0500

    add: sub module

---
+++ sub.py
@@ -0,0 +1,13 @@
+""""Subtraction Module"""
+def sub(minuend: float, subtrahend: float)-> float:
+    """_summary_
+
+    Args:
+        minuend (float): _description_
+        subtrahend (float): _description_
+
+    Returns:
+        float: solution to problem
+    """    
+    solution = minuend - subtrahend
+    return solution
