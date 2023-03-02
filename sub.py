f42c8b08bb20fcc8b1c392d38a6ae4d2b73df22b
codenamejupiterx <behal1@morgan.edu>
2023-03-02 12:16:34 -0500

    Add-sub-module (#4)
    * add: sub module
    
    * update documentation

---
+++ sub.py
@@ -0,0 +1,13 @@
+""""Subtraction Module"""
+def sub(minuend: float, subtrahend: float)-> float:
+    """_summary_
+
+    Args:
+        minuend (float): the number being subtracted from
+        subtrahend (float): the number being subtracted from another
+
+    Returns:
+        float: solution to problem
+    """    
+    solution = minuend - subtrahend
+    return solution
